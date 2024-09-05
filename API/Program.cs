784bf4902d70ef9f9583759ce6f181a497985ef6
Gabo Melchor <gabo.melchor@hotmail.com>
2024-09-05 07:22:55 -0600

    Parcial 01 - ADD CORS

--- API/Program.cs
+++ API/Program.cs
@@ -9,8 +9,18 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+//Configure the HTTP reques pipeline
+app.UseCors((cors) => cors
+.AllowAnyHeader()
+.AllowAnyMethod()
+.WithOrigins(
+    "http://localhost:4200",
+    "https://localhost:4200"
+    ));
 
 app.MapControllers();
 
