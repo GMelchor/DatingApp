fe622857907ec1c0cf7a30e79e1b9fc30162a786
Gabo Melchor <gabo.melchor@hotmail.com>
2024-09-25 07:45:13 -0600

    Parcial01-Account component

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  private accountService = inject(AccountService);
+  loggedIn = false;
+  model: any = {};
 
+  login(): void {
+    this.accountService.login(this.model).subscribe({
+      next: (response) => {
+        console.group(response);
+        this.loggedIn = true;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    })
+  }
 }
