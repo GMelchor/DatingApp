82dbc0aa2dc2c439ea7fd2e8e2c0e4ff44fc1670
practicantearq01 <techie11@innovatechminds.com>
2024-10-22 22:19:59 -0600

    Parcial 02 - Update user Controller and add memberResponse

---
+++ API/Data/IUserRepository.cs
@@ -0,0 +1,16 @@
+namespace API.Data;
+
+using API.DataEntities;
+using API.DTOs;
+using API.Entities;
+
+public interface IUserRepository
+{
+    void Update(AppUser user);
+    Task<bool> SaveAllAsync();
+    Task<IEnumerable<AppUser>> GetAllAsync();
+    Task<AppUser?> GetByIdAsync(int id);
+    Task<AppUser?> GetByUsernameAsync(string username);
+    Task<IEnumerable<MemberResponse>> GetMembersAsync();
+    Task<MemberResponse?> GetMemberAsync(string username);
+}
