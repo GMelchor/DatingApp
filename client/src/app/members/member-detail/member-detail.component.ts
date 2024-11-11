f092172220bf1303ee208a8ca7dad21966a7e032
practicantearq01 <techie11@innovatechminds.com>
2024-11-10 23:07:45 -0600

    Parcial 03 - Add style to member detail component

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, OnInit } from "@angular/core";
 import { MembersService } from "../../_services/members.service";
 import { ActivatedRoute } from "@angular/router";
 import { Member } from "../../_models/member";
+import { TabsModule } from "ngx-bootstrap/tabs";
 
 @Component({
   selector: "app-member-detail",
   standalone: true,
-  imports: [],
+  imports: [TabsModule],
   templateUrl: "./member-detail.component.html",
   styleUrl: "./member-detail.component.css",
 })
@@ -19,11 +20,11 @@ export class MemberDetailComponent implements OnInit {
     this.loadMember();
   }
 
-  loadMember(){
+  loadMember() {
     const username = this.route.snapshot.paramMap.get("username");
-    if(!username)return;
+    if (!username) return;
     this.memberService.getMember(username).subscribe({
-      next: member => this.member = member
+      next: (member) => (this.member = member),
     });
   }
 }
